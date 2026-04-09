1553db4db718425367c8a38f514f05bed38b4c47
PaulsDubults <112948827+PaulsDubults@users.noreply.github.com>
2026-04-09 12:16:43 +0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,5 @@
+# Šī rinda importē funkciju create_app no mūsu lietotnes pakotnes.
+from app import create_app
+
+# Šī rinda izveido Flask lietotnes objektu, kuru izmantos Flask komanda.
+app = create_app()
